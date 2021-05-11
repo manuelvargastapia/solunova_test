528538e8ae4ddaf8ea4d2cbd03ff976bf9f9a69d
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-11 19:00:59 -0400

    create initial Sequelize setup

---
+++ backend/src/models/index.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/db_config.js')[env];
+const db = {};
+
+let sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        port: config.port,
+        dialect: config.dialect,
+        logging: false,
+    }
+);
+
+fs.readdirSync(__dirname)
+    .filter((file) => {
+        return (
+            file.indexOf('.') !== 0 &&
+            file !== basename &&
+            file.slice(-3) === '.js'
+        );
+    })
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file))(
+            sequelize,
+            Sequelize.DataTypes
+        );
+        db[model.name] = model;
+    });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
