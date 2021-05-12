7e6f50ba06cfa2cb191437094252075ac6b3bbc6
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-12 19:12:28 -0400

    implement dummy login

--- backend/src/controllers/loginController.js
+++ backend/src/controllers/loginController.js
@@ -1,7 +1,38 @@
 'use strict';
 
+const { User } = require('../models');
+const ErrorResponse = require('../utils/error');
+
 const login = async (req, res, next) => {
-    res.status(200).send();
+    // Pick existing credential, because it could be the `username`
+    // or the `email`, exclusively
+    const credentials = {};
+    const { email, username, password } = req.body;
+    if (email != undefined) {
+        credentials.email = email;
+    } else {
+        credentials.username = username;
+    }
+
+    try {
+        // Filter by username or email accordingly
+        const existingUser = await User.findOne({ where: credentials });
+
+        if (!existingUser) {
+            return next(new ErrorResponse(404, 'User not found'));
+        }
+
+        if (!existingUser.isPasswordValid(password)) {
+            return next(new ErrorResponse(401, 'Invalid credentials'));
+        }
+
+        return res.status(200).json({
+            success: true,
+            token: 'asdasdasdasd',
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = { login };
