b9a1bbebcabd704af67d1a4d7222d83c30aafde1
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-12 17:43:25 -0400

    encrypt password before saving to DB

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const { Model } = require('sequelize');
+const bcrypt = require('bcrypt');
 
 module.exports = (sequelize, DataTypes) => {
     class User extends Model {
@@ -29,6 +30,12 @@ module.exports = (sequelize, DataTypes) => {
         {
             sequelize,
             modelName: 'User',
+            hooks: {
+                beforeCreate: (user) => {
+                    const salt = bcrypt.genSaltSync(10);
+                    user.password = bcrypt.hashSync(user.password, salt);
+                },
+            },
         }
     );
 
