a19a99d3811c9afaa5f16c79cfd5be6c3a90e0b9
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-12 19:08:32 -0400

    use login middleware

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -2,7 +2,8 @@
 
 const router = require('express').Router();
 const { login } = require('../controllers/loginController');
+const loginValidator = require('../middlewares/login_validator');
 
-router.post('/login', login);
+router.post('/login', loginValidator, login);
 
 module.exports = router;
