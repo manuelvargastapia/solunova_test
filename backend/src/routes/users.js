56c26c4c90147b7ed82712776eccc04f18edf40e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-12 22:49:09 -0400

    user JWT header middleware

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -2,9 +2,10 @@
 
 const router = require('express').Router();
 const registerUserValidator = require('../middlewares/register_user_validator');
+const jwtHeaderValidator = require('../middlewares/jwt_header_validator');
 const { createUser, getUser } = require('../controllers/userController');
 
-router.get('/users', getUser);
+router.get('/users', jwtHeaderValidator, getUser);
 
 router.post('/users', registerUserValidator, createUser);
 
