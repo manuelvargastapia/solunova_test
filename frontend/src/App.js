2b3c4d10a54dafd5806c348790cc3c229833fefb
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-13 19:35:37 -0400

    add dynamic background and game logo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,11 +8,26 @@ import {
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import logoImg from './assets/images/logo.png';
+import classes from './App.module.css';
 
 const App = () => {
+    const [imageNumber, setImageNumber] = useState(1);
+
+    useEffect(() => {
+        setInterval(() => {
+            setImageNumber(Math.floor(Math.random() * 7) + 1);
+        }, 5000);
+    }, []);
+
     return (
-        <Router>
-            <div>
+        <main
+            className={classes.app}
+            style={{
+                backgroundImage: `url("/images/background_${imageNumber}.png")`,
+            }}
+        >
+            <Router>
                 <Switch>
                     <Route exact path="/">
                         <Redirect to="/login" />
@@ -20,8 +36,13 @@ const App = () => {
                     <Route exact path="/register" component={Register} />
                     <Route exact path="/home" component={Home} />
                 </Switch>
-            </div>
-        </Router>
+            </Router>
+            <img
+                className={classes['app-logo']}
+                src={logoImg}
+                alt="Horizon Zero Dawn logo"
+            />
+        </main>
     );
 };
 
