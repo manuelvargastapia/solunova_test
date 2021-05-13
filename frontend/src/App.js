949f16819805d520608758e56dc8e3a07f86219e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-13 13:07:07 -0400

    create Router and Switch + define Login as main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
-function App() {
-    return <p>Hello</p>;
-}
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+} from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+
+const App = () => {
+    return (
+        <Router>
+            <div className="app">
+                <Switch>
+                    <Route exact path="/">
+                        <Redirect to="/login" />
+                    </Route>
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} />
+                    <Route exact path="/home" component={Home} />
+                </Switch>
+            </div>
+        </Router>
+    );
+};
 
 export default App;
