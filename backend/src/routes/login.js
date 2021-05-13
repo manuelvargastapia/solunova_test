995ece6edfe328772965fbb5ed5cffc3bbe39c4a
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-12 22:07:18 -0400

    use sessionLogger as middleware right after login

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const router = require('express').Router();
-const { login } = require('../controllers/loginController');
+const { login, sessionLogger } = require('../controllers/loginController');
 const loginValidator = require('../middlewares/login_validator');
 
-router.post('/login', loginValidator, login);
+router.post('/login', loginValidator, login, sessionLogger);
 
 module.exports = router;
