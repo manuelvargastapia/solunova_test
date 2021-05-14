f1683a8e5510ce6912f75c0de1d448be2bef60e1
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-14 00:00:33 -0400

    update react-spring dependency and useTransition implementation

--- frontend/src/components/Layout/BackgroundImage/index.js
+++ frontend/src/components/Layout/BackgroundImage/index.js
@@ -2,19 +2,9 @@ import { useState, useEffect } from 'react';
 import { useTransition, animated, config } from 'react-spring';
 import classes from './index.module.css';
 
-const images = [
-    { id: 0, url: '/images/background_1.png' },
-    { id: 1, url: '/images/background_2.png' },
-    { id: 2, url: '/images/background_3.png' },
-    { id: 3, url: '/images/background_4.png' },
-    { id: 4, url: '/images/background_5.png' },
-    { id: 5, url: '/images/background_6.png' },
-    { id: 6, url: '/images/background_7.png' },
-];
-
 const BackgroundImage = () => {
-    const [imageIndex, setImageIndex] = useState(0);
-    const transitions = useTransition(images[imageIndex], (item) => item.id, {
+    const [imageNumber, setImageNumber] = useState(1);
+    const transitions = useTransition(`/images/background_${imageNumber}.png`, {
         from: { opacity: 0 },
         enter: { opacity: 1 },
         leave: { opacity: 0 },
@@ -23,17 +13,16 @@ const BackgroundImage = () => {
 
     useEffect(() => {
         setInterval(() => {
-            setImageIndex(Math.floor(Math.random() * 7));
+            setImageNumber(Math.floor(Math.random() * 7) + 1);
         }, 20000);
     }, []);
 
-    return transitions.map(({ item, props, key }) => (
+    return transitions(({ opacity }, item) => (
         <animated.div
-            key={key}
             className={classes['background-img']}
             style={{
-                ...props,
-                backgroundImage: `url("${item.url}")`,
+                opacity,
+                backgroundImage: `url("${item}")`,
             }}
         />
     ));
