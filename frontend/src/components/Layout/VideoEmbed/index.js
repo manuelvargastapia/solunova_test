e100f3127c36e158bf86bb9d23640788e84981ce
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-14 16:43:20 -0400

    Revert "replace iframe with video component"
    This reverts commit 72d79e2c302c4b3651e12f4f733b90b77e0f36f0.

--- frontend/src/components/Layout/VideoEmbed/index.js
+++ frontend/src/components/Layout/VideoEmbed/index.js
@@ -1,16 +1,13 @@
 import classes from './index.module.css';
 
 const VideoEmbed = () => (
-    <video
-        className={classes.video}
-        autoPlay
-        muted
-        loop
-        src="/videos/trailer.mp4"
-        type="video/mp4"
-    >
-        Your browser does not support HTML5 video.
-    </video>
+    <iframe
+        className={classes.iframe}
+        src="https://www.youtube.com/embed/u4-FCsiF5x4?autoplay=1&mute=1&controls=0&playlist=u4-FCsiF5x4&loop=1"
+        allow="autoplay"
+        frameBorder="0"
+        title="Horizon Zero Dawn - E3 2016 Trailer I PS4"
+    />
 );
 
 export default VideoEmbed;
