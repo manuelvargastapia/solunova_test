d28ffc85d85a07c94c1eb0556f31033e7847b285
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-13 23:36:48 -0400

    use newly created BackgroundImage and BackgroundLogo components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -8,25 +7,13 @@ import {
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
-import logoImg from './assets/images/logo.png';
+import BackgroundLogo from './components/Layout/BackgroundLogo';
+import BackgroundImage from './components/Layout/BackgroundImage';
 import classes from './App.module.css';
 
 const App = () => {
-    const [imageNumber, setImageNumber] = useState(1);
-
-    useEffect(() => {
-        setInterval(() => {
-            setImageNumber(Math.floor(Math.random() * 7) + 1);
-        }, 5000);
-    }, []);
-
     return (
-        <main
-            className={classes.app}
-            style={{
-                backgroundImage: `url("/images/background_${imageNumber}.png")`,
-            }}
-        >
+        <main className={classes.app}>
             <Router>
                 <Switch>
                     <Route exact path="/">
@@ -37,11 +24,8 @@ const App = () => {
                     <Route exact path="/home" component={Home} />
                 </Switch>
             </Router>
-            <img
-                className={classes['app-logo']}
-                src={logoImg}
-                alt="Horizon Zero Dawn logo"
-            />
+            <BackgroundLogo />
+            <BackgroundImage />
         </main>
     );
 };
