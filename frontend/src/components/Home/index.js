e23a78df8ab478294ac67bd521c9ad81d66098a1
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-15 18:41:57 -0400

    compose Home component

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -2,7 +2,16 @@ import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 import ErrorMessage from '../UI/ErrorMessage';
-import Card from '../UI/Card';
+import BackgroundLogo from '../Layout/BackgroundLogo';
+import Menu from './Menu';
+import MenuItem from './MenuItem';
+import UserInfo from './UserInfo';
+import UserInfoItem from './UserInfoItem';
+import LogoutButton from './LogoutButton';
+import gameplayIcon from '../../assets/images/icon_gameplay.png';
+import featuresIcon from '../../assets/images/icon_features.png';
+import communityIcon from '../../assets/images/icon_community.png';
+import newsIcon from '../../assets/images/icon_news.png';
 import classes from './index.module.css';
 
 const Home = () => {
@@ -47,18 +56,27 @@ const Home = () => {
     ) : (
         <section className={classes.home}>
             {userInfo && (
-                <Card>
-                    <div>Username: {userInfo.username}</div>
-                    <div>Email: {userInfo.email}</div>
-                    <div>
-                        Name: {userInfo.firstname} {userInfo.lastname}
-                    </div>
-                    <div>Uuid: {userInfo.uuid}</div>
-                    <div>Register time: {userInfo.createdAt}</div>
-                    <div>Last update: {userInfo.updatedAt}</div>
-                    <button onClick={logoutHandler}>Logout</button>
-                </Card>
+                <UserInfo>
+                    <UserInfoItem label="Username" value={userInfo.username} />
+                    <UserInfoItem label="Email" value={userInfo.email} />
+                    <UserInfoItem
+                        label="Name"
+                        value={`${userInfo.firstname} ${userInfo.lastname}`}
+                    />
+                    <UserInfoItem
+                        label="Register time"
+                        value={userInfo.createdAt}
+                    />
+                </UserInfo>
             )}
+            <Menu>
+                <MenuItem iconSrc={gameplayIcon} title="gameplay" />
+                <MenuItem iconSrc={featuresIcon} title="features" />
+                <MenuItem iconSrc={communityIcon} title="community" />
+                <MenuItem iconSrc={newsIcon} title="news" />
+                <LogoutButton onClick={logoutHandler} />
+            </Menu>
+            <BackgroundLogo className={classes['home-logo']} />
         </section>
     );
 };
