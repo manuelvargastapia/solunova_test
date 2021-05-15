dcf262f253975ed4ae0408d529fcbfedf83f1a54
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-15 18:44:33 -0400

    remove Card component

--- frontend/src/components/UI/Card/index.js
+++
@@ -1,7 +0,0 @@
-import classes from './index.module.css';
-
-const Card = ({ children, className }) => (
-    <div className={`${classes.card} ${className}`}>{children}</div>
-);
-
-export default Card;
