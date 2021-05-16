26a20a486c135207500d344057957ad506272035
manuelvargastapia <manuelvargastapia@gmail.com>
2021-05-15 21:43:34 -0400

    remove unused component

--- frontend/src/components/Layout/Quote/index.js
+++
@@ -1,9 +0,0 @@
-import classes from './index.module.css';
-
-const Quote = ({ text }) => (
-    <section className={classes.quote}>
-        <h2>"{text}"</h2>
-    </section>
-);
-
-export default Quote;
